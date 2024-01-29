e31c4722d1cf56712d15d65070f9c4b7b0a64709
Kim Janssens <kim.janssens@viavictor.com>
2024-01-29 08:33:08 +0100

    Refactor tailwind  config purge name

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import forms from '@tailwindcss/forms'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {}
