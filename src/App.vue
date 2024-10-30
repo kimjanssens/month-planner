2254f789fa54f35e9d516844c3b3a0f935d36dc0
Kim Janssens <kimjanssens21@gmail.com>
2024-10-30 10:31:43 +0100

    Remove old logic

--- src/App.vue
+++ src/App.vue
@@ -2,43 +2,7 @@
   <div class="grid grid-cols-3 w-full h-screen">
     <div class="p-8">
       <h2 class="text-lg font-medium uppercase mb-4">Planning Info</h2>
-
       <calendar-form v-model="form" @calculate="refreshTable++" class="mb-20" />
-
-      <h2 class="text-lg font-medium uppercase mb-4">Overview</h2>
-      <ul>
-        <li
-          v-for="(employee, i) in employeesWorkingDays"
-          :key="i"
-          class="grid grid-cols-5 p-2 bg-slate-50 odd:bg-slate-200 cursor-pointer hover:bg-blue-200"
-          :class="{
-            'border-l-4 border-l-blue-600 text-blue-600': employee.name === activeUser?.name
-          }"
-          @click="setActiveUser(employee)"
-        >
-          <div>{{ employee.name }}:</div>
-          <div>
-            {{ employee.days.length }}
-          </div>
-          <div class="col-span-3 flex justify-end">
-            <svg
-              v-if="employee.workDays.length > 0 || employee.holidays.length > 0"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="{1.5}"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-              />
-            </svg>
-          </div>
-        </li>
-      </ul>
     </div>
 
     <div class="col-span-2 grid grid-cols-7">
@@ -64,7 +28,7 @@
 </template>
 
 <script setup>
-import { computed, onMounted, reactive, ref, watch } from 'vue'
+import { computed, reactive, ref } from 'vue'
 
 import CalendarDay from '@/components/CalendarDay.vue'
 import CalendarForm from '@/components/CalendarForm.vue'
@@ -166,79 +130,4 @@ const month = computed(() => {
   return planning
 })
 
-const totalPartTimeWorkingDays = computed(() => {
-  let count = 0
-
-  form.employees.map((employee) => {
-    count += employee.workDays ? employee.workDays.length : 0
-  })
-
-  return count
-})
-
-const employeesWorkingDays = computed(() => {
-  return form.employees.map((employee) => {
-    return {
-      ...employee,
-      days: month.value
-        .filter((day) => day.employees.includes(employee.name))
-        .map((d) => d.dayNumber)
-    }
-  })
-})
-
-const shuffle = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[array[i], array[j]] = [array[j], array[i]]
-  }
-  return array
-}
-
-const setActiveUser = (employee) => {
-  if (activeUser.value?.name === employee.name) {
-    activeUser.value = null
-    return
-  }
-
-  activeUser.value = employee
-}
-
-// First click adds the employee to the day.
-// Second click removes the employee from the day.
-// Third click resets  the day.
-const toggleActiveUserWorkday = (day) => {
-  if (!activeUser.value) return
-
-  form.employees = form.employees.map((employee) => {
-    if (employee.name !== activeUser.value.name) {
-      return employee
-    }
-
-    if (employee.workDays.includes(day.dayNumber)) {
-      employee.workDays = employee.workDays.filter((d) => d !== day.dayNumber)
-      employee.holidays.push(day.dayNumber)
-    } else if (employee.holidays.includes(day.dayNumber)) {
-      employee.holidays = employee.holidays.filter((d) => d !== day.dayNumber)
-    } else {
-      employee.workDays.push(day.dayNumber)
-    }
-
-    return employee
-  })
-}
-
-onMounted(() => {
-  if (localStorage.getItem('calendarEmployees')) {
-    form.employees = JSON.parse(localStorage.getItem('calendarEmployees'))
-  }
-})
-
-watch(
-  () => form.employees,
-  (employees) => {
-    localStorage.setItem('calendarEmployees', JSON.stringify(employees))
-  },
-  { deep: true }
-)
 </script>
--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,33 +1,12 @@
 <template>
   <div
     class="p-4 bg-slate-50 odd:bg-slate-200 relative"
-    :class="{
-      [daysOfTheWeek[day.dayOfTheWeek]?.tailwind]: day.dayNumber === 1,
-      'cursor-pointer': activeUser
-    }"
     @click="$emit('selected-day', day)"
   >
     <div
       class="font-medium text-7xl opacity-20 absolute right-0 bottom-0"
-      :class="{
-        'text-green-400 opacity-40': day.employees.includes(activeUser?.name),
-        'text-red-400 opacity-40': employees.find(
-          (e) =>
-            e.name === activeUser?.name &&
-            e.holidays.includes(day.dayNumber) &&
-            !e.workDays.includes(day.dayNumber)
-        )
-      }"
     >
       {{ day.dayNumber }}
-    </div>
-
-    <div>
-      <ul>
-        <li v-for="employee in day.employees" :key="employee">
-          {{ employee }}
-        </li>
-      </ul>
     </div>
   </div>
 </template>
@@ -38,18 +17,6 @@ defineProps({
     type: Object,
     required: true
   },
-  daysOfTheWeek: {
-    type: Object,
-    required: true
-  },
-  activeUser: {
-    type: Object,
-    default: null
-  },
-  employees: {
-    type: Array,
-    required: true
-  }
 })
 
 defineEmits(['selected-day'])
